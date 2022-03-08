e6cc42ddbae2d8a39208742ab445f049cd486d15
Maksim Dryutov <max_005_27@inbox.lv>
2022-03-09 00:21:27 +0300

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './App.css';
 import { useRoutes } from './routes';
 
 function App() {
   
   const routes = useRoutes();
-
+  const queryClient = new QueryClient();
   
   return (
-    <div className="main">
-      {routes}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="main">
+        <h4> ECMAScript FOR TABLE</h4>
+        {routes}
+      </div>
+    </QueryClientProvider>
+
   );
 }
 
--- src/components/SelectTable.tsx
+++ src/components/SelectTable.tsx
@@ -1,48 +1,69 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useDataFill } from "../hooks/getdb.hook";
-
+import React, { useEffect, useState } from "react";
+import { useQuery} from "react-query";
+const smallTextUrl = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
 
 export const SelectTable = () => {
 
-  const { getData } = useDataFill();
-  const initialState = {
-    bigSize: false,
-    normalSize: false
-  };
-  const [size, setSize] = useState(initialState)
-  const handleClick = (size: {bigSize: boolean, normalSize: boolean}) => {
-    getData({big: size.bigSize});
+  const { data } = useQuery("employees", () => fetch(smallTextUrl)
+    .then((res) => res.json())
+    );
+  
+  const [focusElement, setFocusElement] = useState(0);
+  const arrowUTF = '➪';
+  const focusElements = [ 
+    { id: 'normal_table' }, 
+    { id: 'big_table' }
+  ];
+  
+  console.log(data)
+  const setFocusOnDomElement = () => {
+    const item = document.getElementById(focusElements[focusElement].id);
+      if (!item) return;
+      item.focus();
+  }
+  
+
+  useEffect(() => {
+    setFocusOnDomElement(); 
+  });
+  
+  const handleClick = (e: any, focus: number) => {
+    setFocusElement(focus)
+    setFocusOnDomElement();
+  }
+
+  const focusOnElement = (code: string) => {
+
+    const totalElements = focusElements.length;
+
+    switch(code) {
+      case 'ArrowDown' : {
+        setFocusElement(prev => prev === totalElements - 1 ? 0 : prev + 1);
+        break;
+      }
+      case 'ArrowUp' : {
+        setFocusElement(prev => prev === 0 ? totalElements - 1 : prev - 1);
+        break;
+      }
+      default : return;
+    }
+  }
+
+  const handleKeyDown = (e: any) => {
+    focusOnElement(e.code);
   }
 
 
   return (
     <>
-    <div>
-      <input 
-      id="normalSize" 
-      type="radio" 
-      checked={size.normalSize} 
-      onChange={() => setSize(() => ({...initialState, normalSize: true}))}/>
-      <label htmlFor="bigSize">Normal Table</label>
-    </div>
-    
-    <div>
-      <input 
-      id="bigSize" 
-      type="radio" 
-      checked={size.bigSize} 
-      onChange={() => setSize(() => ({...initialState, bigSize: true}))}/>
-      <label htmlFor="bigSize">Big Table</label>
-    </div>
-
-    <Link to="/table">
-      <button 
-        disabled={!(size.normalSize || size.bigSize)} 
-        onClick={() => handleClick(size)}
-      >Show Table
-      </button>
-    </Link>
+      <div onKeyDown={handleKeyDown}>
+        <div tabIndex={1} id="normal_table" onClick={(e) => handleClick(e, 0)}>
+          <span> { (focusElement === 0) ? arrowUTF : '' } NORMAL TABLE </span>
+        </div>  
+        <div tabIndex={2} id="big_table" onClick={(e) => handleClick(e, 1)}>
+          <span> { (focusElement === 1) ? arrowUTF : '' } BIG TABLE </span>
+        </div>   
+      </div>
     </>
   )
 }
